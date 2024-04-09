37c7e4ba8d22157e952d1f77a59d98f99e71dcab
sigmammal <luisahill456@gmail.com>
2024-04-10 00:12:03 +0100

    price_paths and testing
    Wrote price_paths generator, tested with some generic values. Seems to be working fine

--- index.py
+++ index.py
@@ -19,17 +19,17 @@ class monteCarloSim:
         dt = self.T / self.intervals  # defines the change in time in the given time left
         price_paths = np.zeros((self.epochs + 1, self.intervals))  # initialise empty matrix
         for i in range(self.epochs + 1):
-            z_values = np.random.randn(self.epochs)  # get random Z values using standard normal
-            for z_index in range(len(Z_values)):
-                price_paths[i][z_index] = self.S * np.exp(
-                    (self.mu - 0.5 * (self.sigma ** 2)) * dt + self.sigma * z_values[
-                        z_index])  # use GBM solution to return price paths
+            z_values = np.random.randn(self.intervals)  # get random Z values using standard normal
+            for z_index in range(self.intervals):
+                price_paths[i][z_index] = self.S * np.exp((self.mu - 0.5 * (self.sigma ** 2)) * dt + self.sigma * z_values[z_index])  # use GBM solution to return price paths
         return price_paths
 
 
-# Downloading financial data using yfinance
-ticker_data = yf.Ticker('AAPL')
-historical_data = ticker_data.history(period="5y")  # Retrieving 5 years of historical data
+spot_price = 100
+average = 0.05
+volatility = 0.2
+time_to_maturity = 1
 
-# Displaying the first few rows of the data
-print(historical_data.head())
+simulator = monteCarloSim(spot_price, average, volatility, 100, time_to_maturity, 1)
+prices = simulator.price_paths()
+print(prices)
