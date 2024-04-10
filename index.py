e1af5cfb047687b7ca83f7a67f1f1a4aa168831e
sigmammal <luisahill456@gmail.com>
2024-04-10 23:24:25 +0100

    Call price method
    Implemented call price method, fixed init and paths method

--- index.py
+++ index.py
@@ -7,9 +7,10 @@ class monteCarloSim:
     Object for returning call/put price at given t for European option.
     '''
 
-    def __init__(self, spot_price, mu, sigma, epochs, time_to_maturity, intervals):
+    def __init__(self, spot_price, mu, sigma, epochs, time_to_maturity, intervals, strike_price):
         self.S = spot_price
         self.mu = mu
+        self.strike_price = strike_price
         self.sigma = sigma
         self.epochs = epochs
         self.T = time_to_maturity
@@ -21,15 +22,27 @@ class monteCarloSim:
         for i in range(self.epochs + 1):
             z_values = np.random.randn(self.intervals)  # get random Z values using standard normal
             for z_index in range(self.intervals):
-                price_paths[i][z_index] = self.S * np.exp((self.mu - 0.5 * (self.sigma ** 2)) * dt + self.sigma * z_values[z_index])  # use GBM solution to return price paths
+                price_paths[i][z_index] = self.S * np.exp(
+                    (self.mu - 0.5 * (self.sigma ** 2)) * dt + self.sigma * z_values[
+                        z_index])  # use GBM solution to return price paths
         return price_paths
+
+    def call_price(self, price_matrix):
+        payoffs = [max(price_matrix[-1][i] - self.strike_price, 0) for i in range(self.intervals)]
+        return np.exp(-self.mu * self.T) * 1 / self.intervals * np.sum(payoffs)
 
 
 spot_price = 100
 average = 0.05
 volatility = 0.2
 time_to_maturity = 1
+strike_price = 100
 
-simulator = monteCarloSim(spot_price, average, volatility, 100, time_to_maturity, 1)
-prices = simulator.price_paths()
-print(prices)
+simulator = monteCarloSim(spot_price, average, volatility, 100, time_to_maturity, 100, strike_price)
+s = []
+for i in range(100):
+    prices = simulator.price_paths()
+    s.append(simulator.call_price(prices))
+
+print(s)
+print(np.mean(s))
